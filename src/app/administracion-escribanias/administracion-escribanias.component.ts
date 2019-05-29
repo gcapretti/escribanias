41deb8740b9344252d18536baa11650beec1dfe7
gcapretti <gaston6504@hotmail.com>
2019-05-29 16:24:01 +0200

    se elimina boton de registrarse y se trabaja con la pantalla de administracion de escribanias

--- src/app/administracion-escribanias/administracion-escribanias.component.ts
+++ src/app/administracion-escribanias/administracion-escribanias.component.ts
@@ -2,16 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
 import {SelectionModel} from '@angular/cdk/collections';
 
-export interface PeriodicElement {
-  id: number;
-  nombreEscribania: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 1, nombreEscribania: 'Lopez'},
-  {id: 2, nombreEscribania: 'Pereyra'},
-  {id: 3, nombreEscribania: 'Gonzalez'},
-];
 
 @Component({
   selector: 'app-administracion-escribanias',
@@ -20,29 +11,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class AdministracionEscribaniasComponent {
-  displayedColumns: string[] = ['select', 'id', 'nombreEscribania'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach(row => this.selection.select(row));
-  }
 
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
-  }
 }
