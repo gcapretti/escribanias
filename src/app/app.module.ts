51f5a048ffa9570e66c517149cf637a0a6e508f7
mazcuiFede <matias.azcui@gmail.com>
2019-05-31 20:10:08 -0300

    Ramiro (#1)
    * desinstalo flex layout
    
    * instalo bootstrap + pantallas
    
    * Se sube pantalla de alta de estudio. Se agrego componente de detalle de estudio. Se agrego en Assets archivo JSON de users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout'
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,21 +9,22 @@ import { MaterialModule } from './material.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AdministracionEscribaniasComponent } from './administracion-escribanias/administracion-escribanias.component';
+import { DetalleEscribaniaComponent } from './detalle-escribania/detalle-escribania.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    AdministracionEscribaniasComponent
+    AdministracionEscribaniasComponent,
+    DetalleEscribaniaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule,
-    FlexLayoutModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
