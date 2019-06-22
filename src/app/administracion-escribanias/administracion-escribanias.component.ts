2b2fb40cb4327bac3b76f89f388c902305798014
rares-ctech <48453643+rares-ctech@users.noreply.github.com>
2019-06-22 14:56:45 -0300

    Ramiro (#4)
    * desinstalo flex layout
    
    * instalo bootstrap + pantallas
    
    * Se sube pantalla de alta de estudio. Se agrego componente de detalle de estudio. Se agrego en Assets archivo JSON de users
    
    * Se agregan cambios en la vista del panel de administracion
    
    * Se muestran los datos dinamicamente segun lo que se encuentra en la variable del .ts de adminsitracion-escribanias
    
    * Se obtienen datos de una variable en el .js
    
    * revertimos pisada

--- src/app/administracion-escribanias/administracion-escribanias.component.ts
+++ src/app/administracion-escribanias/administracion-escribanias.component.ts
@@ -24,6 +24,50 @@ export class AdministracionEscribaniasComponent {
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
 
+  listaEscribanias = [
+    {
+      "cuit": "20-25102569-3",
+      "razonsocial": "Estudio Lopez",
+      "direccion": "Corrientes 557 4to B",
+      "telefono": "1168472710",
+      "actividad": "Penal",
+      "provincia": "Buenos Aires",
+      "ciudad": "CABA", 
+      "email": "estudiolopez@gmail.com"
+    },
+    {
+      "cuit": "30-28107984-3",
+      "razonsocial": "Saenz SA",
+      "direccion": "Juan Domingo Peron 1220",
+      "telefono": "1154891113",
+      "actividad": "Civil",
+      "provincia": "Cordoba",
+      "ciudad": "Cordoba", 
+      "email": "contacto@saenzsa.com.ar"
+    },
+    {
+      "cuit": "27-20669523-3",
+      "razonsocial": "Anchorena Legales",
+      "direccion": "Ayacucho 120",
+      "telefono": "1188451361",
+      "actividad": "Laboral",
+      "provincia": "Tucuman",
+      "ciudad": "San Miguel de Tucuman", 
+      "email": "marisa.anchorena@yahoo.com.ar"
+    },
+    {
+      "cuit": "20-17291397-1",
+      "razonsocial": "Burlando Fernando Andres",
+      "direccion": "Alicia Moreau De Justo 740 16vo A",
+      "telefono": "1143428039",
+      "actividad": "Penal",
+      "provincia": "Buenos Aires",
+      "ciudad": "Capital Federal", 
+      "email": "info@burlandito.com"
+    }
+    ];
+  
+
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -44,5 +88,51 @@ export class AdministracionEscribaniasComponent {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
+
+  leerDatos() {
+    let jsonEscribanias = [
+      {
+        "cuit": "20-25102569-3",
+        "razonsocial": "Estudio Lopez",
+        "direccion": "Corrientes 557 4to B",
+        "telefono": "1168472710",
+        "actividad": "Penal",
+        "provincia": "Buenos Aires",
+        "ciudad": "CABA", 
+        "email": "estudiolopez@gmail.com"
+      },
+      {
+        "cuit": "30-28107984-3",
+        "razonsocial": "Saenz SA",
+        "direccion": "Juan Domingo Peron 1220",
+        "telefono": "1154891113",
+        "actividad": "Civil",
+        "provincia": "Cordoba",
+        "ciudad": "Cordoba", 
+        "email": "contacto@saenzsa.com.ar"
+      },
+      {
+        "cuit": "27-20669523-3",
+        "razonsocial": "Anchorena Legales",
+        "direccion": "Ayacucho 120",
+        "telefono": "1188451361",
+        "actividad": "Laboral",
+        "provincia": "Tucuman",
+        "ciudad": "San Miguel de Tucuman", 
+        "email": "marisa.anchorena@yahoo.com.ar"
+      },
+      {
+        "cuit": "20-17291397-1",
+        "razonsocial": "Burlando Fernando Andres",
+        "direccion": "Alicia Moreau De Justo 740 16vo A",
+        "telefono": "1143428039",
+        "actividad": "Penal",
+        "provincia": "Buenos Aires",
+        "ciudad": "Capital Federal", 
+        "email": "info@burlandito.com"
+      }
+      ]
+
   }
 }
