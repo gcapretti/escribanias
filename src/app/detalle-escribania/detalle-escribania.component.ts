8a4970eb8a87786d71a7c3fa254ca314a5e2a3d3
Joel <joelmbasso@hotmail.com>
2019-07-05 17:28:43 -0300

    Version final demo

--- src/app/detalle-escribania/detalle-escribania.component.ts
+++ src/app/detalle-escribania/detalle-escribania.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { Globals } from '../../globals';
 import { FormsModule } from '@angular/forms';
+import { Router } from "@angular/router";
 
 
 @Component({
@@ -14,7 +15,7 @@ export class DetalleEscribaniaComponent implements OnInit {
   //Desde aquí para json global
   listaEscribanias
   escribania
-  constructor(private globals: Globals, private route: ActivatedRoute) { 
+  constructor(private globals: Globals, private route: ActivatedRoute, private router: Router) { 
     this.listaEscribanias = globals.listaEscribanias
   }
   //Hasta aquí para json global
@@ -23,13 +24,21 @@ export class DetalleEscribaniaComponent implements OnInit {
   ngOnInit() {
     for(let i = 0; i < this.listaEscribanias.length; ++i){
       console.log(this.route.snapshot.paramMap.get("cuit"));
-      if (this.listaEscribanias[i].cuit === this.route.snapshot.paramMap.get("cuit"))
+      if (this.listaEscribanias[i].cuit === this.route.snapshot.queryParamMap.get("cuit"))
       {
-        this.escribania = this.listaEscribanias[i]
+        this.escribania = this.listaEscribanias[i];
+        break;
       }
     }
 
   }
   
 
+  editar(razonsocial){
+ // for(let i = 0; i < this.listaEscribanias.length; ++i){
+  //  if (this.listaEscribanias[i].cuit === this.escribania.cuit) {
+   //   this.listaEscribanias[i].razonsocial = document.getElementById(razonsocial).value;
+  //}
+  this.router.navigate(['/home'])
+  }
 }
